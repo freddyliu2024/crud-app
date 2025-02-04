de3b15214d884c453adae9882498096c62e4a411
freddyliu2024 <160204550+freddyliu2024@users.noreply.github.com>
2025-02-04 17:31:39 +0800

    feat: complete dapp anchor crud functions

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -2,69 +2,117 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+declare_id!("EU4HRU8XEvF3HhzmUvJ9n4o8oNxRAF9TLMHB8Yhytb9d");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 #[program]
 pub mod crudapp {
-    use super::*;
+  use super::*;
 
-  pub fn close(_ctx: Context<CloseCrudapp>) -> Result<()> {
+  // Create a new journal entry
+  pub fn create_journal_entry(
+    context: Context<CreateJournalEntry>,
+    title: String,
+    message: String
+  ) -> Result<()> {
+    let journal_entry = &mut context.accounts.journal_entry;
+    journal_entry.owner = *context.accounts.owner.key;
+    journal_entry.title = title;
+    journal_entry.message = message;
+
     Ok(())
   }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crudapp.count = ctx.accounts.crudapp.count.checked_sub(1).unwrap();
+  // Update a journal entry
+  pub fn update_journal_entry(
+    context: Context<UpdateJournalEntry>,
+    _title: String,
+    message: String
+  ) -> Result<()> {
+    let journal_entry = &mut context.accounts.journal_entry;
+    journal_entry.message = message;
+
     Ok(())
   }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crudapp.count = ctx.accounts.crudapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
+  // Delete a journal entry
+  pub fn delete_journal_entry(
+    _context: Context<DeleteJournalEntry>,
+    _title: String
+  ) -> Result<()> {
+    // context.accounts.journal_entry.owner = Pubkey::default();
+    // context.accounts.journal_entry.title = "".to_string();
+    // context.accounts.journal_entry.message = "".to_string();
 
-  pub fn initialize(_ctx: Context<InitializeCrudapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.crudapp.count = value.clone();
     Ok(())
   }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCrudapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
+#[instruction(title: String)]
+pub struct CreateJournalEntry<'info> {
   #[account(
-  init,
-  space = 8 + Crudapp::INIT_SPACE,
-  payer = payer
+    init,
+    payer = owner,
+    space = ANCHOR_DISCRIMINATOR_SIZE + JournalEntryState::INIT_SPACE,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
   )]
-  pub crudapp: Account<'info, Crudapp>,
+  pub journal_entry: Account<'info, JournalEntryState>,
+
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
   pub system_program: Program<'info, System>,
 }
-#[derive(Accounts)]
-pub struct CloseCrudapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
 
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateJournalEntry<'info> {
   #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
+    mut,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    realloc = ANCHOR_DISCRIMINATOR_SIZE + JournalEntryState::INIT_SPACE,
+    realloc::payer = owner,
+    // INFO: To change the original space back to zero and do reallocation of the space
+    realloc::zero = true,
   )]
-  pub crudapp: Account<'info, Crudapp>,
+  pub journal_entry: Account<'info, JournalEntryState>,
+
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+#[instruction(title: String)]
+pub struct DeleteJournalEntry<'info> {
+  #[account(
+    mut,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    // INFO: To close the account if the public key that is specified in the seeds is the same as the signer of the instruction
+    close = owner,
+  )]
+  pub journal_entry: Account<'info, JournalEntryState>,
+
   #[account(mut)]
-  pub crudapp: Account<'info, Crudapp>,
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Crudapp {
-  count: u8,
+pub struct JournalEntryState {
+  pub owner: Pubkey,
+
+  #[max_len(50)]
+  pub title: String,
+
+  #[max_len(1000)]
+  pub message: String,
 }
