58ecb046800e73a59ec6a36fe81993bd829f5f33
freddyliu2024 <160204550+freddyliu2024@users.noreply.github.com>
2025-02-05 10:54:44 +0800

    feat: fe modification

--- src/components/crudapp/crudapp-ui.tsx
+++ src/components/crudapp/crudapp-ui.tsx
@@ -1,64 +1,103 @@
-'use client'
+"use client";
 
-import { Keypair, PublicKey } from '@solana/web3.js'
-import { useMemo } from 'react'
-import { ellipsify } from '../ui/ui-layout'
-import { ExplorerLink } from '../cluster/cluster-ui'
-import { useCrudappProgram, useCrudappProgramAccount } from './crudapp-data-access'
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { ellipsify } from "../ui/ui-layout";
+import { ExplorerLink } from "../cluster/cluster-ui";
+import {
+  useCrudappProgram,
+  useCrudappProgramAccount,
+} from "./crudapp-data-access";
+import { useState } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export function CrudappCreate() {
-  const { initialize } = useCrudappProgram()
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const { createJournalEntry } = useCrudappProgram();
+  const { publicKey } = useWallet();
+
+  const isFormValid = title.trim() !== "" && message.trim() !== "";
+
+  const onSubmit = () => {
+    if (publicKey && isFormValid) {
+      createJournalEntry.mutateAsync({ title, message, owner: publicKey });
+    }
+  };
+
+  if (!publicKey) {
+    return <p>Connect your wallet</p>;
+  }
 
   return (
-    <button
-      className="btn btn-xs lg:btn-md btn-primary"
-      onClick={() => initialize.mutateAsync(Keypair.generate())}
-      disabled={initialize.isPending}
-    >
-      Create {initialize.isPending && '...'}
-    </button>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="input input-bordered w-full max-w-xs"
+      />
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        className="textarea textarea-bordered w-full max-w-xs"
+      />
+      <button
+        onClick={onSubmit}
+        disabled={createJournalEntry.isPending || !isFormValid}
+        className="btn btn-xs lg:btn-md btn-primary"
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export function CrudappList() {
-  const { accounts, getProgramAccount } = useCrudappProgram()
+  const { accounts, getProgramAccount } = useCrudappProgram();
 
   if (getProgramAccount.isLoading) {
-    return <span className="loading loading-spinner loading-lg"></span>
+    return <span className="loading loading-spinner loading-lg"></span>;
   }
   if (!getProgramAccount.data?.value) {
     return (
       <div className="alert alert-info flex justify-center">
-        <span>Program account not found. Make sure you have deployed the program and are on the correct cluster.</span>
+        <span>
+          Program account not found. Make sure you have deployed the program and
+          are on the correct cluster.
+        </span>
       </div>
-    )
+    );
   }
   return (
-    <div className={'space-y-6'}>
+    <div className={"space-y-6"}>
       {accounts.isLoading ? (
         <span className="loading loading-spinner loading-lg"></span>
       ) : accounts.data?.length ? (
         <div className="grid md:grid-cols-2 gap-4">
           {accounts.data?.map((account) => (
-            <CrudappCard key={account.publicKey.toString()} account={account.publicKey} />
+            <CrudappCard
+              key={account.publicKey.toString()}
+              account={account.publicKey}
+            />
           ))}
         </div>
       ) : (
         <div className="text-center">
-          <h2 className={'text-2xl'}>No accounts</h2>
+          <h2 className={"text-2xl"}>No accounts</h2>
           No accounts found. Create one above to get started.
         </div>
       )}
     </div>
-  )
+  );
 }
 
 function CrudappCard({ account }: { account: PublicKey }) {
-  const { accountQuery, incrementMutation, setMutation, decrementMutation, closeMutation } = useCrudappProgramAccount({
-    account,
-  })
-
-  const count = useMemo(() => accountQuery.data?.count ?? 0, [accountQuery.data?.count])
+  const { accountQuery, updateJournalEntry, deleteJournalEntry } =
+    useCrudappProgramAccount({
+      account,
+    });
 
   return accountQuery.isLoading ? (
     <span className="loading loading-spinner loading-lg"></span>
@@ -66,7 +105,10 @@ function CrudappCard({ account }: { account: PublicKey }) {
     <div className="card card-bordered border-base-300 border-4 text-neutral-content">
       <div className="card-body items-center text-center">
         <div className="space-y-6">
-          <h2 className="card-title justify-center text-3xl cursor-pointer" onClick={() => accountQuery.refetch()}>
+          <h2
+            className="card-title justify-center text-3xl cursor-pointer"
+            onClick={() => accountQuery.refetch()}
+          >
             {count}
           </h2>
           <div className="card-actions justify-around">
@@ -80,11 +122,18 @@ function CrudappCard({ account }: { account: PublicKey }) {
             <button
               className="btn btn-xs lg:btn-md btn-outline"
               onClick={() => {
-                const value = window.prompt('Set value to:', count.toString() ?? '0')
-                if (!value || parseInt(value) === count || isNaN(parseInt(value))) {
-                  return
+                const value = window.prompt(
+                  "Set value to:",
+                  count.toString() ?? "0"
+                );
+                if (
+                  !value ||
+                  parseInt(value) === count ||
+                  isNaN(parseInt(value))
+                ) {
+                  return;
                 }
-                return setMutation.mutateAsync(parseInt(value))
+                return setMutation.mutateAsync(parseInt(value));
               }}
               disabled={setMutation.isPending}
             >
@@ -100,15 +149,22 @@ function CrudappCard({ account }: { account: PublicKey }) {
           </div>
           <div className="text-center space-y-4">
             <p>
-              <ExplorerLink path={`account/${account}`} label={ellipsify(account.toString())} />
+              <ExplorerLink
+                path={`account/${account}`}
+                label={ellipsify(account.toString())}
+              />
             </p>
             <button
               className="btn btn-xs btn-secondary btn-outline"
               onClick={() => {
-                if (!window.confirm('Are you sure you want to close this account?')) {
-                  return
+                if (
+                  !window.confirm(
+                    "Are you sure you want to close this account?"
+                  )
+                ) {
+                  return;
                 }
-                return closeMutation.mutateAsync()
+                return closeMutation.mutateAsync();
               }}
               disabled={closeMutation.isPending}
             >
@@ -118,5 +174,5 @@ function CrudappCard({ account }: { account: PublicKey }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
